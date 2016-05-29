54e0f16afb7101c27bfd6dd8a9ebce5e46294fc9
Daniel <danipozo@correo.ugr.es>
2016-05-29 04:46:16 +0200

    DONE:  - Added 3% barrier in DHont method.  - Function to run elections system on a single region and export to pdf.  - Established first simulation option for this.  - Treated data region-level.
    TODO for third day:
     - Treat data country-level, or load every region file and do necessary
       calculations.
     - Put real data on graphs in order to compare it with the calculated.
       Otherwise, they are pointless. 3% barrier treatment modifications
       may be needed for this.
     - Simple gtkmm GUI that takes region name input, runs Julia program
       and launches evince as a previewer.

--- main.jl
+++ main.jl
@@ -1,8 +1,10 @@
 using DataFrames, Gadfly
 
 function DHont(ne::Int64, np::Int64, vp::Array{Float64,1}, vt::Float64)
-    e = [0 for i=1:np]    
     a = [vp[i] for i=1:np]
+    filter!(v->v/vt >= 0.03, a)
+    (s,) = size(a)
+    e = [0 for i=1:s]
 
     for i=1:ne
         (x,j) = findmax(a)
@@ -13,5 +15,34 @@ function DHont(ne::Int64, np::Int64, vp::Array{Float64,1}, vt::Float64)
     return e
 end
 
-n1 = readtable("nombres_partidos.csv", header=false, encoding=:utf8)
-n = [n1[1,i] for i=1:21]
+
+function ProcessRegion(region)
+    region = join(["CSV/", ""], region)
+    rFile = join([region, ""], ".csv")
+    t = readtable(rFile, header=false)
+
+    (f,c) = size(t)
+    ne = parse(Int64, t[f,1])
+    np = div(c-8,2)
+    vp = [parse(Float64, t[f-1,i]) for i=9:2:(c-1)]
+    vt = parse(Float64, t[f-1,5])
+
+    cand = [t[1,i] for i=9:2:(c-1)]
+    
+    cand_vp = [[vp[i], cand[i]] for i=1:np]
+    
+    filter!(v->v[1]/vt >= 0.03, cand_vp)
+    (s,) = size(cand_vp)
+
+    cand = [cand_vp[i][2] for i=1:s]
+
+    e = DHont(ne, np, vp, vt)
+
+    return cand, e
+end
+
+if(ARGS[1] == "--comunidad")
+    c,e = ProcessRegion(ARGS[2])
+    draw(PDF(join([ARGS[2], ""], ".pdf"), 4inch, 3inch), plot(x=c, y=e, Geom.bar, Theme(bar_spacing=0.2inch), Guide.ylabel("Escaños asignados"), Guide.xlabel("Candidaturas")))
+end
+
